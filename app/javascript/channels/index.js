2df5dcd46ecd5270a299504f01f9311a09884e7b
Naokichi55 <ibosanganba@gmail.com>
2025-09-27 19:06:26 +0900

    app/javascript/channels/index.jsを編集。import./consumerを追記

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,6 +1,6 @@
 // Import all the channels to be used by Action Cable
 //AI講師に確認し記述内容を変更するために以下をコメントアウト。
-import "consumer"
+import "./consumer"
 import "./comments_channel"
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
