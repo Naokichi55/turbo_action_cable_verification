f11d62897560d6fb75d4a60bad0a741c9a409a8e
Naokichi55 <ibosanganba@gmail.com>
2025-10-01 23:02:59 +0900

    add:config/routes.rbに追記

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  #action_cableの行を追加
+  mount ActionCable.server => '/cable'
+  
   # Defines the root path route ("/")
   # TOPページへ遷移するように変更
   root "static_pages#top"
