920153d1ba2c9923409061ed53e9ae8bf4f1f62e
Naokichi55 <ibosanganba@gmail.com>
2025-11-06 00:27:43 +0900

    add:コードリーディングの内容追記

--- app/javascript/channels/comments_channel.js
+++ app/javascript/channels/comments_channel.js
@@ -1,6 +1,7 @@
 import consumer from "./consumer"
 
 document.addEventListener('DOMContentLoaded', function() {
+  //constにて変数を定義
   const racketElement = document.querySelector('[data-racket-id]');
   const racketId = racketElement?.dataset.racketId;
 
@@ -16,17 +17,17 @@ if(racketId) {
     console.log("Connected to comments channel for racket")
     // Called when the subscription is ready for use on the server
   },
-
+//接続が切断された時の処理。デバッグとし記録。
   disconnected() {
     console.log("Disconnected from comments channel")
     // Called when the subscription has been terminated by the server
   },
-
+//データを受け取った時の処理について記載。
   received(data) {
      console.log("Received data:", data); 
      //　リアルタイムでコメントを追加
-     //table-commentをracket-idに変更すると投稿がワンクリックで2こ反映される。
-    const commentsContainer = document.getElementById('table-comment')
+     //table-commentをracket-idに変更すると投稿がワンクリックで2回コメントが投稿される。
+    const commentsContainer = document.getElementById('racket-id')
     console.log("Comments container:", commentsContainer);
 
     if(commentsContainer && data.comment) {
